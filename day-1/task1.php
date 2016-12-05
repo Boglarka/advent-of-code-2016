6ba00b67b5bdfcffc89104738c521cb59ba8f33a
Boglarka Weltz <weltz.boglarka@schibsted.hu>
2016-12-06 00:08:12 +0100

    First day - first task

---
+++ day-1/task1.php
@@ -0,0 +1,52 @@
+<?php
+
+$direction = 0; //North
+$north = 0;
+$west = 0;
+
+$puzzle = 'R4, R1, L2, R1, L1, L1, R1, L5, R1, R5, L2, R3, L3, L4, R4, R4, R3, L5, L1, R5, R3, L4, R1, R5, L1, R3, L2, R3, R1, L4, L1, R1, L1, L5, R1, L2, R2, L3, L5, R1, R5, L1, R188, L3, R2, R52, R5, L3, R79, L1, R5, R186, R2, R1, L3, L5, L2, R2, R4, R5, R5, L5, L4, R5, R3, L4, R4, L4, L4, R5, L4, L3, L1, L4, R1, R2, L5, R3, L4, R3, L3, L5, R1, R1, L3, R2, R1, R2, R2, L4, R5, R1, R3, R2, L2, L2, L1, R2, L1, L3, R5, R1, R4, R5, R2, R2, R4, R4, R1, L3, R4, L2, R2, R1, R3, L5, R5, R2, R5, L1, R2, R4, L1, R5, L3, L3, R1, L4, R2, L2, R1, L1, R4, R3, L2, L3, R3, L2, R1, L4, R5, L1, R5, L2, L1, L5, L2, L5, L2, L4, L2, R3';
+$puzzle = explode(', ', $puzzle);
+
+foreach ($puzzle as $i) {
+	$step = substr($i, 1);
+	if ($i[0] == 'L') {
+		$direction--;
+	} else {
+		$direction++;
+	}
+
+	switch ($direction % 4) {
+		// North
+		case 0:
+			$north += $step;
+			break;
+		// East
+		case 1:
+			$west -= $step;
+			break;
+		// South
+		case 2:
+			$north -= $step;
+			break;
+		// West
+		case 3:
+			$west += $step;
+			break;
+		// West
+		case -1:
+			$west += $step;
+			break;
+		// South
+		case -2:
+			$north -= $step;
+			break;
+		// East
+		case -3:
+			$west -= $step;
+			break;
+	} 
+	//echo "puzzle: $i[0]$step\n";
+	//echo "direction: $direction\nnorth: $north\nwest: $west\nblocks away: ".(abs($north)+abs($west))."\n";
+}
+
+echo "direction: $direction\nnorth: $north\nwest: $west\nblocks away: ".(abs($north)+abs($west))."\n";
